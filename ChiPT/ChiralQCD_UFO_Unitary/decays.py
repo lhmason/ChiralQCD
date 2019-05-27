62bda8aa37225e2026116444957e085dbd8d0caf
Lara Mason <lara.mason@live.com>
2019-05-27 09:25:50 +0200

    Lag not working yet

--- ChiPT/ChiralQCD_UFO_Unitary/decays.py
+++
@@ -1,9 +0,0 @@
-# This file was automatically created by FeynRules 2.3.32
-# Mathematica version: 11.3.0 for Mac OS X x86 (64-bit) (March 7, 2018)
-# Date: Wed 15 May 2019 14:03:14
-
-
-from object_library import all_decays, Decay
-import particles as P
-
-
--- ChiralQCD_UFO_Unitary/decays.py
+++
@@ -1,9 +0,0 @@
-# This file was automatically created by FeynRules 2.3.32
-# Mathematica version: 11.3.0 for Mac OS X x86 (64-bit) (March 7, 2018)
-# Date: Fri 17 May 2019 08:21:15
-
-
-from object_library import all_decays, Decay
-import particles as P
-
-

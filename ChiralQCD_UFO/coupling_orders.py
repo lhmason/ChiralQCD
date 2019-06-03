07e3fa74b524c6f14f26238568fdcc18a7a4d840
Lara Mason <lara.mason@live.com>
2019-06-03 11:32:40 +0200

    small update to matrices not right yet

---
+++ ChiralQCD_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 11.3.0 for Mac OS X x86 (64-bit) (March 7, 2018)
+# Date: Thu 30 May 2019 15:03:40
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
